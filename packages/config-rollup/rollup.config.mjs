50fa0dc5e98cb62f584c746b002de029e513b08d
hamo-o <yheel369@gmail.com>
2024-04-30 18:53:07 +0900

    fix: React를 읽지 못하는 오류 수정

--- packages/config-rollup/rollup.config.mjs
+++ packages/config-rollup/rollup.config.mjs
@@ -1,34 +1,41 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import json from '@rollup/plugin-json';
-import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import css from "rollup-plugin-import-css";
+import json from "@rollup/plugin-json";
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
 export function makeRollupConfig(packageJSON, options = {}) {
   return {
-    input: packageJSON.main,
+    input: packageJSON.input,
     output: {
       file: packageJSON.module,
-      format: 'es',
+      format: "es",
     },
     external: [/@babel\/runtime/],
     plugins: [
+      commonjs({ include: "node_modules/**" }),
       babel({
         presets: [
-          '@babel/preset-env',
-          '@babel/preset-react',
-          '@babel/preset-typescript',
+          "@babel/preset-env",
+          [
+            "@babel/preset-react",
+            {
+              runtime: "automatic",
+            },
+          ],
+          "@babel/preset-typescript",
         ],
-        plugins: ['@babel/plugin-transform-runtime'],
-        babelHelpers: 'runtime',
-        exclude: 'node_modules/**',
+        plugins: ["@babel/plugin-transform-runtime"],
+        babelHelpers: "runtime",
+        exclude: "node_modules/**",
         extensions,
       }),
       resolve({ extensions }),
-      commonjs({ include: 'node_modules/**' }),
       peerDepsExternal(),
       json(),
+      css(),
     ],
     ...options,
   };
